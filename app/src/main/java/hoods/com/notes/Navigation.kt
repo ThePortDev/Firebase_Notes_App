af4f5ae334307a8a17d5872685220470179d4091
ThePortDev <111320669+ThePortDev@users.noreply.github.com>
2023-11-01 16:02:54 -0600

    Post-Nav Fix

--- app/src/main/java/hoods/com/notes/Navigation.kt
+++ app/src/main/java/hoods/com/notes/Navigation.kt
@@ -1,15 +1,10 @@
 package hoods.com.notes
 
 import androidx.compose.runtime.Composable
-import androidx.navigation.NavGraph
-import androidx.navigation.NavGraphBuilder
-import androidx.navigation.NavHostController
-import androidx.navigation.NavType
+import androidx.navigation.*
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
-import androidx.navigation.navigation
 import hoods.com.notes.detail.DetailScreen
 import hoods.com.notes.detail.DetailViewModel
 import hoods.com.notes.home.Home
@@ -17,9 +12,8 @@ import hoods.com.notes.home.HomeViewModel
 import hoods.com.notes.login.LoginScreen
 import hoods.com.notes.login.LoginViewModel
 import hoods.com.notes.login.SignUpScreen
-import kotlin.math.log
 
-enum class LoginRoutes{
+enum class LoginRoutes {
     Signup,
     SignIn
 }
@@ -33,12 +27,14 @@ enum class NestedRoutes {
     Main,
     Login
 }
+
+
 @Composable
 fun Navigation(
     navController: NavHostController = rememberNavController(),
     loginViewModel: LoginViewModel,
     detailViewModel: DetailViewModel,
-    homeViewModel: HomeViewModel,
+    homeViewModel: HomeViewModel
 ) {
     NavHost(
         navController = navController,
@@ -51,6 +47,8 @@ fun Navigation(
             homeViewModel
         )
     }
+
+
 }
 
 fun NavGraphBuilder.authGraph(
@@ -62,18 +60,18 @@ fun NavGraphBuilder.authGraph(
         route = NestedRoutes.Login.name
     ) {
         composable(route = LoginRoutes.SignIn.name) {
-            LoginScreen(
-                onNavToHomePage = {
-                    navController.navigate(NestedRoutes.Main.name) {
-                        launchSingleTop = true
-                        popUpTo(route = LoginRoutes.SignIn.name) {
-                            inclusive = true
-                        }
-                    }
-                },
-                loginViewModel = loginViewModel
-            ) {
+            LoginScreen(onNavToHomePage = {
                 navController.navigate(NestedRoutes.Main.name) {
+                    launchSingleTop = true
+                    popUpTo(route = LoginRoutes.SignIn.name) {
+                        inclusive = true
+                    }
+                }
+            },
+                loginViewModel = loginViewModel
+
+            ) {
+                navController.navigate(LoginRoutes.Signup.name) {
                     launchSingleTop = true
                     popUpTo(LoginRoutes.SignIn.name) {
                         inclusive = true
@@ -82,20 +80,17 @@ fun NavGraphBuilder.authGraph(
             }
         }
 
-        composable(route =  LoginRoutes.Signup.name) {
+        composable(route = LoginRoutes.Signup.name) {
             SignUpScreen(onNavToHomePage = {
-                navController.navigate(HomeRoutes.Home.name) {
+                navController.navigate(NestedRoutes.Main.name) {
                     popUpTo(LoginRoutes.Signup.name) {
                         inclusive = true
                     }
                 }
-            }, loginViewModel = loginViewModel
+            },
+                loginViewModel = loginViewModel
             ) {
-                navController.navigate(HomeRoutes.Home.name) {
-                    popUpTo(LoginRoutes.Signup.name) {
-                        inclusive = true
-                    }
-                }
+                navController.navigate(LoginRoutes.SignIn.name)
             }
         }
     }
@@ -105,18 +100,18 @@ fun NavGraphBuilder.homeGraph(
     navController: NavHostController,
     detailViewModel: DetailViewModel,
     homeViewModel: HomeViewModel
-) {
+){
     navigation(
         startDestination = HomeRoutes.Home.name,
         route = NestedRoutes.Main.name,
-    ) {
-        composable(HomeRoutes.Home.name) {
+    ){
+        composable(HomeRoutes.Home.name){
             Home(
                 homeViewModel = homeViewModel,
                 onNoteClick = { noteId ->
                     navController.navigate(
                         HomeRoutes.Detail.name + "?id=$noteId"
-                    ) {
+                    ){
                         launchSingleTop = true
                     }
                 },
@@ -124,22 +119,23 @@ fun NavGraphBuilder.homeGraph(
                     navController.navigate(HomeRoutes.Detail.name)
                 }
             ) {
-                navController.navigate(NestedRoutes.Login.name) {
+                navController.navigate(NestedRoutes.Login.name){
                     launchSingleTop = true
-                    popUpTo(0) {
+                    popUpTo(0){
                         inclusive = true
                     }
                 }
+
             }
         }
 
         composable(
             route = HomeRoutes.Detail.name + "?id={id}",
-            arguments = listOf(navArgument("id") {
+            arguments = listOf(navArgument("id"){
                 type = NavType.StringType
                 defaultValue = ""
             })
-        ) { entry ->
+        ){ entry ->
             DetailScreen(
                 detailViewModel = detailViewModel,
                 noteId = entry.arguments?.getString("id") as String,
